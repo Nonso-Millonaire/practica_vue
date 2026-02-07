a80bc9bbbf45339383cf2f00fac5d94b67810972
Nonso <edokashopify@gmail.com>
2026-02-07 11:32:39 +0100

    feat: estructura inicial con Laravel y Vue

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Project::create(['name' => 'Sistema de Gestión', 'description' => 'Laravel + Vue']);
+    }
+}
